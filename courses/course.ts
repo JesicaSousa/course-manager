d8076264d962f91149495b4498575446155c3cd7
Jesica Sousa <jesicasousa1997@gmail.com>
2022-07-10 07:01:29 -0300

    deploy

---
+++ courses/course.ts
@@ -0,0 +1,11 @@
+export class Course{
+    id!: number;
+    name!: string;
+    price!: number;
+    code!: string;
+    imageUrl!: string;
+    duration!: number;
+    rating!: number;
+    releaseDate!: string;
+    description!: string;
+}
